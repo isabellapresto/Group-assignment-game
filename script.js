0a08484620e697f00c75dce91544a7b5526e1c51
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2022-10-12 10:10:13 +0200

    Funktion door1 och bilder tillagda i dörr1 och dörr3

--- script.js
+++ script.js
@@ -7,16 +7,24 @@ const btn2 = document.querySelector (".btn2");
 const btn3 = document.querySelector(".btn3");
 const escapebtn = document.querySelector(".escapebtn");
 const textHeader = document.querySelector(".textHeader");
+const imageDoor1 = document.querySelector(".imagedoor1");
+const imageDoor3 = document.querySelector(".imagedoor3");
 
 //Happens when you go in to door 1 
+btn1.addEventListener("click", openDoor1);
 
-
-function walkToBomb(){
-    door1.style.display.none;
-    door2.style.display.none;
-    door3.style.display.none;
-
+function openDoor1(){
+    body.style.backgroundColor = "yellow";
+    door1.style.display = "none";
+    door2.style.display = "none";
+    door3.style.display = "none";
+    escapebtn.style.display = "block";
+    textHeader.innerText = "BOOM wrong turn!!";
+    textHeader.style.color = "red";
+    imageDoor1.style.display ="block";
+    imageDoor3.style.display = "none";
 }
+
 
 //Happens when you go in to door 3
 
@@ -28,9 +36,9 @@ function openDoor3(){
     door2.style.display = "none";
     door3.style.display = "none";
     escapebtn.style.display = "block";
-    back.style.display = "block";
     textHeader.innerText = "No wrong turn!!";
     textHeader.style.color = "red";
+    imageDoor3.style.display = "block";
 }
 
 escapebtn.addEventListener("click", walkToEscape);
@@ -41,6 +49,8 @@ function walkToEscape(){
     door2.style.display = "block";
     door3.style.display = "block";
     escapebtn.style.display = "none";
+    imageDoor1.style.display ="none";
+    imageDoor3.style.display = "none";
     back.style.display = "none";
     textDoor3.innerText = "Let the game begin";
     textDoor3.style.color = "white";
